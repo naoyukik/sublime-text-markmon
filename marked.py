56593e25211644b8880ad344dceb833baa4d046e
Paul Scott <paul.scotto@gmail.com>
2013-07-28 10:47:54 +0100

    PEP8 formatting

--- marked.py
+++ marked.py
@@ -1,17 +1,20 @@
-import os, sys
+import os
+import sys
 import subprocess
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+
 
 class MarkedCommand(sublime_plugin.WindowCommand):
     def run(self):
         filename = self.window.active_view().file_name()
 
         proc_env = os.environ.copy()
-
+        encoding = sys.getfilesystemencoding()
         for k, v in proc_env.items():
-          proc_env[k] = os.path.expandvars(v).encode(sys.getfilesystemencoding())
+            proc_env[k] = os.path.expandvars(v).encode(encoding)
 
         subprocess.call(['open', '-a', 'Marked', filename], env=proc_env)
 
     def is_enabled(self):
-      return True
+        return True
